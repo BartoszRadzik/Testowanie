d987526889d7b21bbfdcdc6cd260179f910ddf75
BartoszRadzik <72854593+BartoszRadzik@users.noreply.github.com>
2021-05-09 10:40:07 +0200

    a
    a

---
+++ Test.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+public class Main{
+	public static void main(String[] args){
+		System.out.println("Witam!\n");
+	}
+	@Test
+	public void testrunTest(){
+		System.out.println("Testowanko");
+
+	}
+
+}
